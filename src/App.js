9181d58deadb26dcc388d6760d5132f99d163285
Eloa Cardozo Augusto <elooa_@hotmail.com>
2021-09-06 22:10:37 -0300

    Adicionar API do github e exibir as informações do usuário encontrado

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
-
 import Form from './components/Form';
-import Github from './components/Github';
 
 function App() {
   return (
     <div className="container text-center">
       <h1 className="py-5 text-uppercase"> Github profile</h1>
       
-      <Form />
-      <Github />
-     
+      <Form />     
 
     </div>
   );
--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,17 +1,53 @@
-const form = () => {
+import React, { useState } from 'react';
+import GitHubImage from './../../assets/img/github.svg'
+
+function Form() {
+    const [search, setSearch] = useState();
+    const [userData, setUserData] = useState();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        fetch(`https://api.github.com/users/${search}`)
+            .then(response => response.json())
+            .then(userResponse => setUserData(userResponse));
+    }
+
+    const handleChange = (event) => {
+        setSearch(event.target.value);
+    }
+
     return (
-        <form>
-            <div className="form-group">
-                <label>Github user:</label>
-                <div className="input-group">
-                    <input type="text" className="form-control" required />
-                    <span className="input-group-btn">
-                        <input type="submit" className="btn btn-success" />
-                    </span>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Github user:</label>
+                    <div className="input-group">
+                        <input type="text" required className="form-control" value={search} onChange={handleChange} />
+                        <span className="input-group-btn">
+                            <input type="submit" className="btn btn-success" />
+                        </span>
+                    </div>
                 </div>
+            </form>
+
+            <div className="py-4">
+                {!userData && (
+                    <img src={GitHubImage} className="responsive rounded-circle" alt="" height="200px" />
+                )}
+                {userData && (
+                    <div>
+                        <img src={userData.avatar_url} className="responsive rounded-circle" alt="" height="200px" />
+
+                        <h1 className="pt-3">
+                            <a href={userData.html_url}> {userData.name} </a>
+                        </h1>
+                        <h3> {userData.location} </h3>
+                    </div>
+                )}
             </div>
-        </form>
+
+        </div>
     );
 }
 
-export default form;
+export default Form;
